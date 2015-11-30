49c3014255fe1153cda739ba52b763f68085dbb6
arcticmatt <mlimcollege@gmail.com>
2015-11-30 15:05:11 -0800

    Do part 2: double spring pendulums

--- hw6/Part1/Double_Spring_Pendulum/double_spring_pendulums_demo.cpp
+++ hw6/Part1/Double_Spring_Pendulum/double_spring_pendulums_demo.cpp
@@ -42,13 +42,13 @@ struct Color
 struct Spring_Pendulum
 {
     float m;
-    
+
     float x;
     float y;
-    
+
     float px;
     float py;
-    
+
     float k;
     float rl;
 };
@@ -108,14 +108,14 @@ void init(void)
     glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_reflect);
     glMaterialfv(GL_FRONT, GL_SPECULAR, specular_reflect);
     glMaterialf(GL_FRONT, GL_SHININESS, shininess);
-    
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    
+
     glOrtho(left_param, right_param,
             bottom_param, top_param,
             near_param, far_param);
-    
+
     glMatrixMode(GL_MODELVIEW);
 }
 
@@ -123,9 +123,9 @@ void reshape(int width, int height)
 {
     height = (height == 0) ? 1 : height;
     width = (width == 0) ? 1 : width;
-    
+
     glViewport(0, 0, width, height);
-    
+
     glutPostRedisplay();
 }
 
@@ -135,15 +135,15 @@ void display(void)
     glLoadIdentity();
 
     glTranslatef(-cam_position[0], -cam_position[1], -cam_position[2]);
-    
+
     trace_path();
-    
+
     glEnable(GL_LIGHTING);
     draw_spring_pendulums();
     glDisable(GL_LIGHTING);
 
     draw_text();
-    
+
     glutSwapBuffers();
 }
 
@@ -161,55 +161,55 @@ float compute_lagrangian()
     float total = ke + pe;
     min_total = (total < min_total) ? total : min_total;
     max_total = (total > max_total) ? total : max_total;
-    
+
     return ke - pe;
 }
 
 void update_path()
-{    
+{
     if(path1.size() == max_num_points)
     {
         path1.erase(path1.begin());
         path2.erase(path2.begin());
-        
+
         path_colors1.erase(path_colors1.begin());
         path_colors2.erase(path_colors2.begin());
     }
-    
+
     Point point1;
     point1.x = m1.x;
     point1.y = m1.y;
-    
+
     Point point2;
     point2.x = m2.x;
     point2.y = m2.y;
-    
+
     float lagrangian_norm = abs(compute_lagrangian() / lagrangian_0);
     lagrangian_norm = (lagrangian_norm > 1.0) ? 1.0 : lagrangian_norm;
-    
+
     Color color1;
     color1.r = lagrangian_norm;
     color1.g = lagrangian_norm;
     color1.b = 1.0 - lagrangian_norm;
-    
+
     Color color2;
     color2.r = 1.0 - lagrangian_norm;
     color2.g = lagrangian_norm;
     color2.b = lagrangian_norm;
-    
+
     path1.push_back(point1);
     path_colors1.push_back(color1);
-    
+
     path2.push_back(point2);
     path_colors2.push_back(color2);
 }
 
 void update_pendulums()
 {
-    /******************************* TODO *******************************/
+    /********************************************************************/
 
     /* Your task is to write some lines of code to update:
-     * 
+     *
      *     m1.x
      *     m1.y
      *     m1.px
@@ -237,41 +237,55 @@ void update_pendulums()
      *     m2.k
      *     m2.rl
      *     g
-     * 
+     *
      */
 
-    
+    /*
+     * To get the continuous Laplacian, we just add the kinetic and potential
+     * energies for each pendulum.
+     *
+     * This is pretty simple. There is only one catch. When calculating the
+     * potential energy of the second (attached) pendulum, we use
+     * sqrt((x2 - x1)^2 + (y2 - y1)^2) for the stretch distance. That is,
+     * we must take into account the fact that its position is affected by
+     * the first pendulum, and that it is not in fact stretched as far as its
+     * position suggests.
+     *
+     * After taking this into account, everything else is fairly straighforward.
+     * We just calculate all 8 momentum variables, and from those, do the updates.
+     */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-    /****************************** END TODO ****************************/
+    /********************************************************************/
 
     t += dt;
+
+    /*** Update momentum variables before updating position variables ***/
+    float sqrt_val1 = sqrt(pow(m1.x, 2) + pow(m1.y, 2));
+    float sqrt_val2 = sqrt(pow(m2.x - m1.x, 2) + pow(m2.y - m1.y, 2));
+    float sqrt_frac1 = (sqrt_val1 - m1.rl) / sqrt_val1;
+    float sqrt_frac2 = (sqrt_val2 - m2.rl) / sqrt_val2;
+
+    // Update momentum for mass 1
+    m1.px += dt * ( -(m1.k * m1.x * sqrt_frac1) + (m2.k * (m2.x - m1.x) * sqrt_frac2) );
+    m1.py += dt * ( -(m1.k * m1.y * sqrt_frac1) + (m2.k * (m2.y - m1.y) * sqrt_frac2)
+                        + m1.m * g );
+
+    // Update momentum for mass 2
+    m2.px += dt * -(m2.k * (m2.x - m1.x) * sqrt_frac2);
+    m2.py += dt * ( -(m2.k * (m2.y - m1.y) * sqrt_frac2) + m2.m * g );
+
+    /*** Use updated momentum variables to update position variables ***/
+    m1.x += (dt * m1.px) / m1.m;
+    m1.y += (dt * m1.py) / m1.m;
+    m2.x += (dt * m2.px) / m2.m;
+    m2.y += (dt * m2.py) / m2.m;
 }
 
 void update()
 {
     update_path();
     update_pendulums();
-    
+
     glutPostRedisplay();
 }
 
@@ -280,7 +294,7 @@ void trace_path()
     glVertexPointer(2, GL_FLOAT, 0, &path1[0]);
     glColorPointer(3, GL_FLOAT, 0, &path_colors1[0]);
     glDrawArrays(GL_POINTS, 0, path1.size());
-    
+
     glVertexPointer(2, GL_FLOAT, 0, &path2[0]);
     glColorPointer(3, GL_FLOAT, 0, &path_colors2[0]);
     glDrawArrays(GL_POINTS, 0, path2.size());
@@ -292,7 +306,7 @@ void draw_spring_pendulums()
     glVertex2f(0, 0);
     glVertex2f(m1.x, m1.y);
     glEnd();
-    
+
     glBegin(GL_LINES);
     glVertex2f(m1.x, m1.y);
     glVertex2f(m2.x, m2.y);
@@ -300,14 +314,14 @@ void draw_spring_pendulums()
 
     float pendulum_radius = 0.3;
     glutSolidSphere(pendulum_radius * 0.5, 20, 20);
-    
+
     glPushMatrix();
     {
         glTranslatef(m1.x, m1.y, 0);
         glutSolidSphere(pendulum_radius, 20, 20);
     }
     glPopMatrix();
-    
+
     glPushMatrix();
     {
         glTranslatef(m2.x, m2.y, 0);
@@ -375,10 +389,10 @@ int main(int argc, char* argv[])
         cerr << "\nERROR: Incorrect number of arguments." << endl;
         exit(1);
     }
-    
+
     int xres = atoi(argv[1]);
     int yres = atoi(argv[2]);
-    
+
     m1.m = 1;
     m1.x = atof(argv[3]);
     m1.y = atof(argv[4]);
@@ -386,7 +400,7 @@ int main(int argc, char* argv[])
     m1.py = 0;
     m1.k = 20;
     m1.rl = 1;
-    
+
     m2.m = 1;
     m2.x = atof(argv[5]);
     m2.y = atof(argv[6]);
@@ -394,15 +408,15 @@ int main(int argc, char* argv[])
     m2.py = 0;
     m2.k = 20;
     m2.rl = 1;
-    
+
     lagrangian_0 = compute_lagrangian();
-    
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(xres, yres);
     glutInitWindowPosition(0, 0);
     glutCreateWindow("Double Spring Pendulum");
-    
+
     init();
     glutDisplayFunc(display);
     glutIdleFunc(update);
