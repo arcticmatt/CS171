0b9b5a261a2dd6534a0db74409fac15dc8c53fbd
arcticmatt <mlimcollege@gmail.com>
2015-12-01 00:08:09 -0800

    Get rotations kind of working

--- hw6/Part2/I_Bar/I-bar_code.cpp
+++ hw6/Part2/I_Bar/I-bar_code.cpp
@@ -8,6 +8,7 @@
  */
 
 #include <stdio.h>
+#include <cmath>
 #include <iostream>
 #include <cassert>
 #include <fstream>
@@ -30,8 +31,13 @@ void drawIBar();
 void key_pressed(unsigned char key, int x, int y);
 void init_keyframes();
 void update_keyframes();
-void interpolate_translation_and_scaling();
+void interpolate_translation_rotation_scaling();
 float interpolate_points(Vector4f u_vec, MatrixXf B, Vector4f p_vec);
+Quaternionf get_quaternion(Vec4f rotation);
+Quaternionf interpolate_quaternions(Quaternionf q1, Quaternionf q2, float u);
+Vec4f quaternion2rotation(Quaternionf q);
+float deg2rad(float angle);
+float rad2deg(float rad);
 
 /* Camera and perspective params (camera angle is 0, so no need for a call
  * to glRotatef for the camera)*/
@@ -56,7 +62,7 @@ int frame_num = 0;
 int keyframe_num = 0;
 Vec3f curr_translation(0, 0, 0);
 Vec3f curr_scale(1, 1, 1);
-Vec4f curr_rotation;
+Vec4f curr_rotation(0, 0, 0, 0);
 Keyframe prev_frame;
 Keyframe curr_frame;
 Keyframe next_frame;
@@ -143,6 +149,7 @@ void drawIBar()
     glPushMatrix();
 
     glTranslatef(curr_translation.x, curr_translation.y, curr_translation.z);
+    glRotatef(curr_rotation.alpha, curr_rotation.x, curr_rotation.y, curr_rotation.z);
     glScalef(curr_scale.x, curr_scale.y, curr_scale.z);
 
     glColor3f(0, 0, 1);
@@ -154,6 +161,7 @@ void drawIBar()
     glPushMatrix();
 
     glTranslatef(curr_translation.x, curr_translation.y, curr_translation.z);
+    glRotatef(curr_rotation.alpha, curr_rotation.x, curr_rotation.y, curr_rotation.z);
     glScalef(curr_scale.x, curr_scale.y, curr_scale.z);
 
     glColor3f(0, 1, 1);
@@ -165,6 +173,7 @@ void drawIBar()
     glPushMatrix();
 
     glTranslatef(curr_translation.x, curr_translation.y, curr_translation.z);
+    glRotatef(curr_rotation.alpha, curr_rotation.x, curr_rotation.y, curr_rotation.z);
     glScalef(curr_scale.x, curr_scale.y, curr_scale.z);
 
     glColor3f(1, 0, 1);
@@ -176,6 +185,7 @@ void drawIBar()
     glPushMatrix();
 
     glTranslatef(curr_translation.x, curr_translation.y, curr_translation.z);
+    glRotatef(curr_rotation.alpha, curr_rotation.x, curr_rotation.y, curr_rotation.z);
     glScalef(curr_scale.x, curr_scale.y, curr_scale.z);
 
     glColor3f(1, 1, 0);
@@ -187,6 +197,7 @@ void drawIBar()
     glPushMatrix();
 
     glTranslatef(curr_translation.x, curr_translation.y, curr_translation.z);
+    glRotatef(curr_rotation.alpha, curr_rotation.x, curr_rotation.y, curr_rotation.z);
     glScalef(curr_scale.x, curr_scale.y, curr_scale.z);
 
     glColor3f(0, 1, 0);
@@ -203,10 +214,11 @@ void key_pressed(unsigned char key, int x, int y) {
     } else if (key == 's') {
         // 's' steps forward one frame
         update_keyframes();
-        interpolate_translation_and_scaling();
+        interpolate_translation_rotation_scaling();
         cout << "New frame num = " << frame_num << endl;
         cout << "New translation = " << curr_translation.to_string() << endl;
         cout << "New scale = " << curr_scale.to_string() << endl;
+        cout << "New rotation = " << curr_rotation.to_string() << endl;
         glutPostRedisplay();
     }
 }
@@ -242,13 +254,16 @@ void update_keyframes() {
 
 /*
  * Use Catmull-Rom to interpolate current translation and scaling vectors.
+ *
+ * Use SLERP to interpolate rotation vectors.
  */
-void interpolate_translation_and_scaling() {
+void interpolate_translation_rotation_scaling() {
     if (frame_num == curr_frame.frame_num) {
         // If we're at a Keyframe, just use the translation and scale vectors
         // of the Keyframe
         curr_translation = curr_frame.translation;
         curr_scale = curr_frame.scale;
+        curr_rotation = curr_frame.rotation;
     } else {
         // Else, interpolate
         MatrixXf B(4, 4);
@@ -284,6 +299,12 @@ void interpolate_translation_and_scaling() {
         Vector4f z_points(prev_frame.scale.z, curr_frame.scale.z,
                 next_frame.scale.z, next_next_frame.scale.z);
         curr_scale.z = interpolate_points(u_vec, B, z_points);
+
+        // Interpolate rotation points
+        Quaternionf q1 = get_quaternion(curr_frame.rotation);
+        Quaternionf q2 = get_quaternion(next_frame.rotation);
+        Quaternionf interpolated_q = interpolate_quaternions(q1, q2, u);
+        curr_rotation = quaternion2rotation(interpolated_q);
     }
 }
 
@@ -298,4 +319,59 @@ void interpolate_translation_and_scaling() {
 float interpolate_points(Vector4f u_vec, MatrixXf B, Vector4f p_vec) {
     float interpolated = u_vec.dot(B * p_vec);
     return interpolated;
+}
+
+/*
+ * Converts a rotation axis and angle into a quaternion.
+ */
+Quaternionf get_quaternion(Vec4f rotation) {
+    Vector3f axis(rotation.x, rotation.y, rotation.z);
+    axis.normalize();
+    float angle = deg2rad(rotation.alpha);
+    float quaternion_real = cos(angle / 2.0);
+    float imaginary_scalar = sin(angle / 2.0);
+    axis = axis * imaginary_scalar;
+    Quaternionf q(quaternion_real, axis(0), axis(1), axis(2));
+    return q;
+}
+
+/*
+ * Use SLERP to interpolate two quaternions.
+ */
+Quaternionf interpolate_quaternions(Quaternionf q1, Quaternionf q2, float u) {
+    float angle = acos(q1.dot(q2));
+    float q1_scalar = (sin((1 - u) * angle) / sin(angle));
+    float q2_scalar = (sin(u * angle) / sin(angle));
+    Vector4f coeffs = q1.coeffs() * q1_scalar + q2.coeffs() * q2_scalar;
+    Quaternionf q(coeffs(0), coeffs(1), coeffs(2), coeffs(3));
+    return q;
+}
+
+/*
+ * Converts a Quaternion to its corresponding rotation vector.
+ */
+Vec4f quaternion2rotation(Quaternionf q) {
+    float half_angle = acos(q.x());
+    float angle = half_angle * 2.0; // in radians
+    Vector4f coeffs = q.coeffs();
+    coeffs /= sin(half_angle);
+    float x = coeffs(0);
+    float y = coeffs(1);
+    float z = coeffs(2);
+    Vec4f rotation(x, y, z, rad2deg(angle));
+    return rotation;
+}
+
+/*
+ * Converts given angle in degrees to radians.
+ */
+float deg2rad(float angle) {
+    return angle * M_PI / 180.0;
+}
+
+/*
+ * Converts given angle in radians to degrees.
+ */
+float rad2deg(float rad) {
+    return rad * 180 / M_PI;
 }
