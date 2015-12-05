a29560657a63828b8aa68dac84002b49517671d4
arcticmatt <mlimcollege@gmail.com>
2015-12-04 19:55:14 -0800

    Add unmodified HW7a files

---
+++ hw7/src/Assignment.cpp
@@ -0,0 +1,14 @@
+#include "Assignment.hpp"
+
+#include "Utilities.hpp"
+
+#include "Scene.hpp"
+#include "UI.hpp"
+
+void Assignment::drawIOTest() {
+
+}
+
+void Assignment::drawIntersectTest(Camera *camera) {
+
+}
